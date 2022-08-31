b680920dbf5022f2741b853a6f9852923142e684
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2022-08-31 11:17:18 +0200

    Add files via upload

---
+++ subscripts/annotate-off-targets.R
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# $NAS/NGS/RefSeq/hg19_both.sort.bed $file.exons-non-coding.targets.tsv
+
+a=read.table(file=args[1],header=F)
+b=read.table(file=args[2],header=F)
+
+com=which(is.element(a[,4],b[,1]))
+
+for (i in com){
+	sel=which(b[,1]==a[i,4])
+	if(length(sel)>0){
+		for(j in 1:length(sel)){
+			a[i,4]=paste(a[i,4],b[sel[j],2],sep=",")
+		}
+	}
+}
+
+write.table(a,file=args[3],quote=F,sep="\t",row.names=F, col.names=F)
+
+
---
+++ subscripts/annotate-targets.R
@@ -0,0 +1,71 @@
+
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+a=read.table(file=args[1],header=F)
+b=read.table(file=args[2],header=F)
+
+#a=read.table(file="/home/mquinodo/SYNO/scripts_NGS_analysis/OFF-PEAK-02/refs/../refs/hg19_ncbiRefSeq.exons.sort.merge.bed",header=F)
+#b=read.table(file="/home/mquinodo/SYNO/scripts_NGS_analysis/OFF-PEAK-02/refs/Twist_ComprehensiveExome_targets_hg19.bed",header=F)
+
+a=cbind(a,a[,4])
+a[,5]="No"
+
+for (i in 1:dim(a)[1]){
+	if(i==1){
+		temp=b[which(b[,1]==a[i,1]),]
+		} else {
+			if(a[i,1]!=a[i-1,1]){
+				temp=b[which(b[,1]==a[i,1]),]
+			}
+		}
+	temp2=temp[which(abs(temp[,2]-a[i,2])<100000),]
+
+	done=0
+	# completely covered
+	overlap=which(temp2[,2]<=a[i,2] & temp2[,3]>=a[i,3])
+	if(length(overlap)>0){
+		overlap=overlap[which.max(temp2[overlap,3]-temp2[overlap,2])]
+	}
+	if(length(overlap)>0){
+		a[i,5]="Yes"
+		done=1
+	}
+
+	# covered only on the left
+	overlap=which(temp2[,2]<=a[i,2] & temp2[,3]>=a[i,2] & temp2[,3]<a[i,3] & done==0) 
+	if(length(overlap)>0){
+		overlap=overlap[which.max(temp2[overlap,3]-temp2[overlap,2])]
+	}
+	if(length(overlap)>0){
+		a[i,5]="Yes"
+		#a[i,3]=temp2[overlap,3]
+		done=1
+	}
+
+	# covered only on the right
+	overlap=which(temp2[,2]<=a[i,3] & temp2[,3]>=a[i,3] & temp2[,2]>a[i,2] & done==0) 
+	if(length(overlap)>0){
+		overlap=overlap[which.max(temp2[overlap,3]-temp2[overlap,2])]
+	}
+	if(length(overlap)>0){
+		a[i,5]="Yes"
+		#a[i,2]=temp2[overlap,2]
+		done=1
+	}
+
+	# covered only in the center
+	overlap=which(temp2[,2]>a[i,2] & temp2[,3]<a[i,3] & done==0) 
+	if(length(overlap)>0){
+		overlap=overlap[which.max(temp2[overlap,3]-temp2[overlap,2])]
+	}
+	if(length(overlap)>0){
+		a[i,5]="Yes"
+		#a[i,2]=temp2[overlap,2]
+		#a[i,3]=temp2[overlap,3]
+	}
+}
+
+write.table(a[which(a[,5]=="Yes"),1:4],file=args[3],quote=F,sep="\t",row.names=F, col.names=F)
+write.table(a[which(a[,5]=="No"),1:4],file=args[4],quote=F,sep="\t",row.names=F, col.names=F)
+

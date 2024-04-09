0092caa7e4d753415f52491327203e5182f4349e
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2024-04-09 10:26:51 +0200

    Update annotate-off-targets.R

--- subscripts/annotate-off-targets.R
+++ subscripts/annotate-off-targets.R
@@ -6,6 +6,12 @@ args = commandArgs(trailingOnly=TRUE)
 a=read.table(file=args[1],header=F)
 b=read.table(file=args[2],header=F)
 
+levelsa = levels (a[[4]])
+levelsb  = levels (b[[1]])
+union_levels <- union (levelsa, levelsb)
+a[,4] <- factor (a[,4], levels=union_levels)
+b[,1] <- factor (b[,1], levels=union_levels)
+
 com=which(is.element(a[,4],b[,1]))
 
 for (i in com){

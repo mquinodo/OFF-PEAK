f56150981b508ed9af8024a12103b2b6405399d5
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2024-04-09 11:03:33 +0200

    Update annotate-off-targets.R

--- subscripts/annotate-off-targets.R
+++ subscripts/annotate-off-targets.R
@@ -6,10 +6,6 @@ args = commandArgs(trailingOnly=TRUE)
 a=read.table(file=args[1],header=F)
 b=read.table(file=args[2],header=F)
 
-lev <- unique(c(a[,4],b[,1]))
-a[,4] <- factor (a[,4], levels=lev)
-b[,1] <- factor (b[,1], levels=lev)
-
 com=which(is.element(a[,4],b[,1]))
 
 for (i in com){
